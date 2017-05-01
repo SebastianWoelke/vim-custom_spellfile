2eb4e37b1fb624e9c841517aca7edeacdc5dab4c
Your Name <you@example.com>
2017-05-01 11:30:44 +0200

    Update install routine for Neovim

--- install.sh
+++ install.sh
@@ -2,6 +2,23 @@
 
 current_dir=$(pwd)
 cd ~/.
-mkdir -p .vim/spell
-ln -sf $current_dir/wordlist_de.utf-8.add ~/.vim/spell/de.utf-8.add 
 
+function configure_vim {
+  mkdir -p .vim/spell
+  ln -sf $current_dir/wordlist_de.utf-8.add ~/.vim/spell/de.utf-8.add 
+}
+
+function configure_nvim {
+  mkdir -p .config/nvim/spell
+  ln -sf $current_dir/wordlist_de.utf-8.add ~/.config/nvim/spell/de.utf-8.add 
+}
+
+if [ "$#" == "1" ]; then
+  if [ "$1" == "nvim" ]; then
+    configure_nvim
+  elif [ "$1" == "vim" ]; then
+    configure_vim
+  fi
+else
+  configure_vim #backword compatibility
+fi
